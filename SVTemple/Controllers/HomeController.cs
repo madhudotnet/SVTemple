09420386674d43adc873adbfe38ff81217a2536a
Madhu <b.madhoo@gmail.com>
2018-09-06 10:18:23 +0530

    Add project files.

---
+++ SVTemple/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SVTemple.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Charity()
+        {
+            ViewBag.Message = "Your Charity page.";
+
+            return View();
+        }
+        public ActionResult History()
+        {
+            ViewBag.Message = "Your History page.";
+
+            return View();
+        }
+        public ActionResult Events()
+        {
+            ViewBag.Message = "Your Events page.";
+
+            return View();
+        }
+    }
+}
---
+++ SVTemple/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SVTemple.MvcApplication" Language="C#" %>
